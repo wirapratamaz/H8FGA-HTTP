bddf887c01902259c5374386c3a6b86944474dba
wirapratamaz <wira@gringgo.co>
2023-04-05 23:07:14 +0800

    add post request

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +10,7 @@ import (
 )
 
 func main() {
+	//* GET
 	//Get request dengan menggunakan function http.Get
 	res, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
 
@@ -30,4 +33,47 @@ func main() {
 	sb := string(body)
 
 	log.Println(sb)
+
+	//* POST
+	//map data
+	data := map[string]interface{}{
+		"title":  "Airell",
+		"body":   "Jordan",
+		"userId": 1,
+	}
+
+	//ubah map data menjadi string JSON -> simpan ke variable requestJson
+	requestJson, err := json.Marshal(data)
+	//instance dari http.Client untuk melakukan request.
+	client := &http.Client{}
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//HTTP request baru dengan menggunakan http.NewRequest()
+
+	req, err := http.NewRequest("POST", "https://jsonplaceholder.typicode.com/posts",
+		//payload data ditampung dalam request body menggunakan bytes.NewBuffer()
+		bytes.NewBuffer(requestJson))
+	//mengatur header
+	req.Header.Set("Content-type", "application/json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//menjalankan request dengan client.Do(req)
+	res, err = client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Menutup body dari responsen
+	defer res.Body.Close()
+
+	//Membaca isi dari responsen
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//cetak argumen yang sudah dikonversi menjadi string
+	log.Println(string(body))
 }
